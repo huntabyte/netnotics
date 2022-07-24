0da512b0bd94be9b15233d81da50a9dc9f4b0ee5
Hunter Johnston <johnstonhuntera@gmail.com>
2022-07-23 20:58:42 -0400

    add interfaces to homepage

--- frontend/src/routes/dashboard/index.svelte
+++ frontend/src/routes/dashboard/index.svelte
@@ -1,7 +1,32 @@
-<script>
-	import title from '$lib/stores/title';
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('http://localhost:8000/devices', {
+			method: 'GET',
+			credentials: 'include'
+		});
 
-	$title = 'Netnotics';
+		const devices = await res.json();
+
+		if (res.ok) {
+			return {
+				props: {
+					deviceCount: devices.length
+				}
+			};
+		}
+		return {
+			status: res.status,
+			error: new Error('Could not fetch devices')
+		};
+	}
 </script>
 
-<h1 class="text-xl">Welcome to Netnotics</h1>
+<script>
+	import title from '$lib/stores/title';
+	export let deviceCount;
+	import StatCard from '$components/StatCard.svelte';
+
+	$title = 'Dashboard';
+</script>
+
+<StatCard title="Devices" link="/devices" value={deviceCount} />
